53fc92f07a9a7d9a0db070611825e06f6c31ae98
olasoMateus <73443539+olasoMateus@users.noreply.github.com>
2022-01-17 10:01:09 -0300

    Add files via upload

---
+++ Lab7.java
@@ -0,0 +1,81 @@
+//Classe que usaremos como Thread
+class T extends Thread {
+    //Identificador da thread, para auxiliar com a soma
+    private int id;
+    //Variável que contém guarda a soma dos elementos contados na Thread
+    private int soma;
+    //Número de Threads, necessário para ordenar as posições que serão contadas pela Thread
+    private int numDeThreads; 
+    //Vetor que será somado;
+    private int[] vetorDeInt;
+  
+    //Construtor
+    public T(int tid, int soma, int numDeThreads, int vetorDeInt[]) { 
+        this.id = tid; 
+        this.soma = soma;
+        this.numDeThreads = numDeThreads;
+        this.vetorDeInt = vetorDeInt;
+    }
+
+    //Método main da thread
+    public void run() {
+        System.out.println("Thread " + this.id + " iniciou!");
+        this.soma = 0;
+        for (int i=this.id; i< vetorDeInt.length; i = i + this.numDeThreads) {
+            this.soma = this.soma + this.vetorDeInt[i];
+            System.out.println("Fazendo -" + this.id + "-" +this.soma); 
+
+        }
+        System.out.println("Thread " + this.id + " terminou!"); 
+    }
+}
+
+//Classe da aplicacao
+class SomaDeVetor {
+    //Número de Threads
+    static final int NThreads = 2;
+    //Tamanho do vetor
+    static final int Tam = 10;
+
+    public static void main (String[] args) {
+        //Reserva espaço para um vetor de threads
+        Thread[] threads = new Thread[NThreads];
+
+        //Cria um vetor de inteiros
+        int[] vetorDeInt = new int[Tam];
+
+        //Cria um vetor que armazena as somas de cada Thread
+        int[] somaDeThread = new int[NThreads];
+
+        //Cria a variável da soma total
+        int somaTotal = 0;
+
+        //Inicializa o vetor de inteiros
+        for (int i=0; i<Tam; i++) {
+            vetorDeInt[i] = 1;
+        }
+
+        //cria as threads da aplicacao
+        for (int i=0; i<NThreads; i++) {
+            threads[i] = new T(i, somaDeThread[i], NThreads, vetorDeInt);
+        }
+
+        //inicia as threads
+        for (int i=0; i<threads.length; i++) {
+            threads[i].start();
+        }
+
+        //espera pelo termino de todas as threads
+        for (int i=0; i<threads.length; i++) {
+            try { 
+                threads[i].join();
+                somaTotal = somaTotal + somaDeThread[i];
+            }
+            catch (InterruptedException e) { 
+                return; 
+            }
+        }
+
+        System.out.println("Valor da soma Total = " + somaTotal); 
+   }
+}
